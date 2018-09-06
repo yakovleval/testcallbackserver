51622d4d0636288b1c2eb102a3f5130ca8a7713c
alex <sanya89_muravlenko@mail.ru>
2018-09-06 17:29:25 +0500

    такс теперь убираем клавиатуру в начале диалога

--- commands/start.py
+++ commands/start.py
@@ -1,7 +1,7 @@
 import command_system
 def start_message(x,y):
     message = 'чтобы начать получать расписание, отправь свой класс'
-    return message
+    return message, False
 
 start_command = command_system.Command()
 start_command.keys = ['начать']
