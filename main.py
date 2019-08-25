20aa1688b379cec8c81c6b16568456cb0df5e7fe
alex <sanya89_muravlenko@mail.ru>
2019-08-25 19:45:46 +0300

    F

--- main.py
+++ main.py
@@ -2,19 +2,18 @@
 from response_creator import send_message
 from flask import Flask, request, json
 import vk
-
+from settings import key, confirmation_token
 
 app = Flask(__name__)
 
-key = '1e81968a155a6b554c34fd710349e7410ecd6804ff58788de3b2d1edb40e0af47cf4671546184958bc905'
 
-confirmation_token = '6071d7c2'
+
+
 
 @app.route('/', methods=['POST'])
 def processing():
-    #Распаковываем json из пришедшего POST-запроса
+
     data = json.loads(request.data)
-    #Вконтакте в своих запросах всегда отправляет поле типа
     if 'type' not in data.keys():
         return 'not vk'
     if data['type'] == 'confirmation':
@@ -23,7 +22,6 @@ def processing():
         print(data)
         user_id = data['object']['from_id']
         user_message = data['object']['text']
-        send_message(user_id, user_message)  # отсылает ответ
-        # Сообщение о том, что обработка прошла успешно
+        send_message(user_id, user_message)
         return 'ok'
 
---
+++ settings.py
@@ -0,0 +1,11 @@
+token = 'a6ce7f02770d6f1c1ccbfc042bd349f9b208338ad1a2314612f5790e51e2f4e085fb6baf45f8f43857f4d'
+
+
+
+host='ec2-54-217-235-137.eu-west-1.compute.amazonaws.com'
+port='5432'
+database='dbi9dq212bg4sq'
+user='yvyloqfqwtebtk'
+password='64092c34e652d59ef86eb0863fe845a4e1cd8f43444d06c25f2024de3e825f1d'
+key = '1e81968a155a6b554c34fd710349e7410ecd6804ff58788de3b2d1edb40e0af47cf4671546184958bc905'
+confirmation_token = '6071d7c2'
